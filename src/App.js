dafd43a67d4d5237835d74e8bdb6cb47e56b78fa
ajinkyabhuruk1996 <ajinkya.91bhuruk@gmail.com>
2022-05-19 17:05:47 +0530

    Initial commit-working form update

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+import React from "react";
+// import ReactDOM from "react-dom";
+
+import MainContent from './components/main'
+
+function App() {
+  return (
+    <div className="App">
+        <MainContent />
+    </div>
+  );
+}
+
+export default App;
